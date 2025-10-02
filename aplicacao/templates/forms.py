d6342c3675c66993e0343b0f7d18778307e8f09e
Rafael Souza <141173714+rafaelrsouza89@users.noreply.github.com>
2025-10-01 22:27:05 -0300

    atualizado

---
+++ aplicacao/templates/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Cliente, Perfil
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ('nome', 'email')
+
+class PerfilForm(forms.ModelForm):
+    class Meta:
+        model = Perfil
+        fields = ('telefone', 'rua', 'numero', 'cep', 'bairro', 'cidade', 'complemento')
+        
