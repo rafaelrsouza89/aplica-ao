6e525f03beff87f7a35f94e507b72d25d40b5e53
Rafael Souza <141173714+rafaelrsouza89@users.noreply.github.com>
2025-10-01 22:29:21 -0300

    atualizado

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('' , views.index, name="url_index"),
+    path('produto', views.produto, name="url_produto"),
+    path('lista_vendas', views.lista_vendas, name="url_lista_vendas"),
+    path('cad_produto', views.cad_produto, name="url_cad_produto"),
+    path('atualizar_produto/<int:id>', views.atualizar_produto, name="url_atualizar_produto"),
+    path('apagar_produto/<int:id>', views.apagar_produto, name="url_apagar_produto"),
+    path('entrar', views.entrar, name="url_entrar"),
+    path('cad_user', views.cad_user, name="url_cad_user"),
+    path('cad_cliente', views.cad_cliente, name="url_cad_cliente"),
+    path('registrar_venda', views.registrar_venda, name="url_registrar_venda"),
+    path('sair', views.sair, name="url_sair"),
 ]
